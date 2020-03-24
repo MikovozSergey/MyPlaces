1a3bd2beafc1a4991080d66f6ed89e306e3d6e11
MikovozSergey <mikovozs98@gmail.com>
2020-03-24 11:17:52 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Дарья Станкевич on 3/24/20.
+//  Copyright © 2020 Sergey Mikovoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
