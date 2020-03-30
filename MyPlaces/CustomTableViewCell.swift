cb5c1ffab34a88e44f237c8c39ba2f631c62e7a1
MikovozSergey <mikovozs98@gmail.com>
2020-03-30 20:08:48 +0300

    edit customTableViewCell for Places

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Дарья Станкевич on 3/30/20.
+//  Copyright © 2020 Sergey Mikovoz. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+   
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+}
