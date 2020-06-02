c6f78e19c7ce725a35e42759e32029fda75ebb08
MikovozSergey <mikovozs98@gmail.com>
2020-06-02 13:49:16 +0300

    add icons for location of person, add location for person + add icon for enter address in location label

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -8,22 +8,50 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
-    var place = Place() 
+    var place = Place()
     let annotationIdentifier = "annotationIdentifier"
+    
+    let locationManager = CLLocationManager() // менеджер который будет отслеживать местоположение пользователя
+    
+    let regionInMeters = 5_000.00
+    
+    var incomeSegueIdentifier = ""
 
     @IBOutlet var mapView: MKMapView!
+    @IBOutlet var mapPinImage: UIImageView!
+    @IBOutlet var adressLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-        setupPlaceMark()
+        setupMapView()
+        checkLocationServices()
     }
     
+    
+    @IBAction func centerViewInUserLocation() { // по нажатию на кнопку геолокации, центрируем карту
+        
+        showUserLocation()
+    }
+    
+    @IBAction func doneButtonPressed() {
+    }
     @IBAction func closeVC() {
         dismiss(animated: true)
+    }
+    
+    private func setupMapView() {
+        
+        if incomeSegueIdentifier == "showPlace" {
+            setupPlaceMark()
+            mapPinImage.isHidden = true
+        }
     }
     
     private func setupPlaceMark() { // устанавливаем месту отметку
@@ -54,6 +82,65 @@ class MapViewController: UIViewController {
             self.mapView.selectAnnotation(annotation, animated: true)
         }
     }
+    
+    private func checkLocationServices() { // метод для проверки включенных на устройстве функций для нахождения геолокации
+        
+        if CLLocationManager.locationServicesEnabled() { // если геолокация доступна
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else { // Алерт контроллер для включения геолокации на устройстве
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(title: "Your location services are disabled", message: "To enable it Go to: Settings -> Privacy -> Location services and turn On")
+            }
+        }
+    }
+    
+    private func setupLocationManager() { // устанавливаем местоположение
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // настраиваем точность определения местопололожения пользователя
+    }
+    
+    private func checkLocationAuthorization() { // проверка статуса на разрешение использовать геопозицию
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse: // геолокация в момент использования
+            mapView.showsUserLocation = true
+            if incomeSegueIdentifier == "getAdress" { showUserLocation() }
+            break
+        case .denied: // когда приложению отказано использовать службу геолокации
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(title: "Your location is not available", message: "To give permission Go to: Setting -> MyPlaces -> Location")
+            }
+            break
+        case .notDetermined: // статус не определен, если пользователь не решил может ли использовать приложение локацию
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted: // приложение не авторизовано для служб геолокаций
+            break
+        case .authorizedAlways: // авторизован всегда
+            break
+        @unknown default:
+            print("New case is available ")
+        }
+    }
+    
+    private func showUserLocation() {
+        
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location,
+                                            latitudinalMeters: regionInMeters,
+                                            longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    private func showAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
 extension MapViewController: MKMapViewDelegate {
@@ -79,5 +166,12 @@ extension MapViewController: MKMapViewDelegate {
         }
         
         return annotationView
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
     }
 }
